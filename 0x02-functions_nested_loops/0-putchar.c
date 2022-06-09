0589c14447950e425a9c11bcf87a08133ee7341a
Olorunfemi-Temitayo <olorunfemig023@gmail.com>
2022-06-09 05:32:18 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+  int n = 0;
+  char str_ptc[8] = "_putchar";
+
+  while (n < 9)
+    {
+      _putchar(str_ptc[n];
+	       n += 1;
+     }
+	_putchar('\n');
+      return (0);
+}
+  
