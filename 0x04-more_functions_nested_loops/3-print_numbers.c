f9f6ec693d9ce1a47c34929f2d785b970c9d9764
Olorunfemi-Temitayo <olorunfemig023@gmail.com>
2022-06-09 03:29:21 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * Return:0
+ */
+
+void print_numbers(void)
+{
+  char n = 0;
+
+  while (n <= 90)
+    {
+      _putchar('0' + n);
+      n++;
+
+    }
+  _putchar('\n');
+
+}
