16d5daa5c731a6f3f318214d9a5e3fd5d42c3fbd
Olorunfemi-Temitayo <101413926+Olorunfemi-Temitayo@users.noreply.github.com>
2022-06-14 12:24:02 +0100

    low level programming

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,28 +1,11 @@
 #include "main.h"
 /**
-*times_table - prints the 9 times table
-*
-*Return: returns nothing
+*add - sums up two numbers
+*@n:first integer
+*@m:second integer
+*Return: returns 0
 */
-void times_table(void)
+int add(int n, int m)
 {
-int digit, mult, result;
-for (digit = 0; digit <= 9; digit++)
-{
-_putchar('0');
-
-for (mult = 1; mult <= 9; mult++)
-{
-_putchar(',');
-_putchar(' ');
-result = digit * mult;
-if (result <= 9)
-_putchar(' ');
-else
-_putchar((result / 10) + '0');
-
-_putchar((result % 10) + '0');
-}
-_putchar('\n');
-}
+return (n + m);
 }
