006575ed06b92a137d37686a80a89f7217a58c89
Olorunfemi-Temitayo <101413926+Olorunfemi-Temitayo@users.noreply.github.com>
2022-06-14 12:40:34 +0100

    low level programming

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
-void positive_negative(int i);
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
