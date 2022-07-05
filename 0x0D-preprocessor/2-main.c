4b26a379af28e7325b9a82dcaea860ce705eaa1c
Olorunfemi-Temitayo <101413926+Olorunfemi-Temitayo@users.noreply.github.com>
2022-07-05 12:23:11 +0100

    main.c file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - prints the name
- *of the file it was compiled from
- *
- *Return: nothing to be returned
- */
+*main - prints the name of
+*the file it was compiled from
+*
+*Return: nothing to be returned
+*/
 
 int main(void)
 {
-  printf("%s\n", __FIK
+printf("%s\n", __FILE__);
+return (0);
+}
