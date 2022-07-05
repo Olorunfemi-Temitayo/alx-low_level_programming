dbc105e7a9ec8f34de146b46f50a2e3ac2548970
Olorunfemi-Temitayo <olorunfemig023@gmail.com>
2022-07-05 04:08:23 -0700

    main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of
+ * the file it was compiled from
+ *
+ * Return: nothing to be returned
+ */
+
+int main(void)
+{
+  printf("%\n",__file___);
+  return(0);
+}
